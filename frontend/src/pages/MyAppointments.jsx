8d20e3071351b4eee220d392efba050b3d0decb1
Pavithravignesh <pvs02799@gmail.com>
2025-11-30 15:06:05 +0530

    route and basic set-up has been completed

---
+++ frontend/src/pages/MyAppointments.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyAppiontment() {
+    return (
+        <div>
+            <h1>MyAppiontment</h1>
+        </div>
+    )
+}
+
+export default MyAppiontment
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,10 @@
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
+import Login from "./Login";
+import Appointments from "./Appointments";
+import Doctors from "./Doctors";
+import MyProfile from "./MyProfile";
+import MyAppiontment from "./MyAppointments";
+
+export { Home, Contact, About, Login, Appointments, MyAppiontment, Doctors, MyProfile }
