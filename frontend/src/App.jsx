733923fc16cd8f5058bac4e7ee321bbb33039dd6
Pavithravignesh <pvs02799@gmail.com>
2025-11-30 18:58:16 +0530

    nav has been completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { About, Contact, Doctors, Home, Login, MyProfile, MyAppiontment, Appointments } from './pages'
+import { Navbar } from './components'
 
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
+      <Navbar />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/doctors' element={<Doctors />} />
